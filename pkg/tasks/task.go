ed36583a4807641faab5d2e62fc60047b8bab93b
rusdevops <rusdevops@gmail.com>
2021-05-26 14:18:42 +0300

    updated Event in tasks pkg

--- pkg/tasks/task.go
+++ pkg/tasks/task.go
@@ -1,3 +1,26 @@
 package tasks
 
-const TaskDificultyBeginer = "beginer"
+// EventType - тип события
+type EventType uint
+
+// DifficultyType - сложность задачи
+type DifficultyType uint
+
+const (
+	DifficultyBegin DifficultyType = iota
+	DifficultyLow
+	DifficultyMedium
+	DifficultyHigh
+)
+
+const (
+	Created EventType = iota
+	Removed
+	Updated
+)
+
+// Event - событие о задачах
+type Event struct {
+	Type EventType
+	Body map[string]interface{}
+}
